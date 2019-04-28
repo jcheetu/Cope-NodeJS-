139c5c4d34deb54ccd75b40eaa2365f798babcba
jcheetu <j.cheetu@gmail.com>
2019-04-28 16:34:23 +0530

    Watson Connector

--- api/controllers/AIController.js
+++ api/controllers/AIController.js
@@ -6,20 +6,41 @@ module.exports = {
     //      ai : "",
     // }
     sendRequest: function(req, res) {
+      console.log(req);
       let data = req.allParams();
       AIService.sendRequest(data, function(output) {
         return res.send(output);
       });
     },
-    loadBot: function(req, res) {
+    loadDialogueflowBot: function(req, res) {
+      console.log(req);
       let data = {};
       data.ai = req.params.ai;
       data.accessToken = req.params.accessToken;
       data.sessionId = req.params.sessionId;
       data.title = req.params.title;
-      AIService.sendRequest(data, function() {
-        return res.view('chatbot/Bot.ejs', data);
-      });
+
+
+      return res.view('chatbot/dialogueflow/Bot.ejs', data);
+      
+      // AIService.sendRequest(data, function() {
+      //   return res.view('chatbot/Bot.ejs', data);
+      // });
+    },
+
+    loadWatsonBot: function(req, res) {
+      let data = {};
+      data.ai = req.params.ai;
+      data.accessToken = req.params.accessToken;
+      data.username = req.params.username;
+      data.password = req.params.password;
+      data.title = req.params.title;
+
+      return res.view('chatbot/watson/Bot.ejs', data);
+
+      // AIService.sendRequest(data, function() {
+      //   return res.view('chatbot/Bot.ejs', data);
+      // });
     }
 
 }
